6d2adb16f48453a674438047b568cd0e1e48abb3
SoyYuri09 <yuri.garcia252583@potros.itson.edu.mx>
2025-10-14 00:16:16 -0700

    Creación del proyecto y sus clases en java

---
+++ code/examen_generar_constancia/src/main/java/main/Examen_generar_constancia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Examen_generar_constancia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
